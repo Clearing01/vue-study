fc9c4cb6e73c87e3aad2749e012c0e02ccc8fac1
minho.park <minho.park@twolinecode.com>
2022-12-31 15:56:43 +0900

    vue-cli 기초

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,19 @@
+<template>
+<!-- HTML(template) -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+    // JavaScript - 인스턴스 옵션
+    method: {
+        addNum: function() {
+            
+        }
+    }
+}
+</script>
+
+<style>
+    /* css */
+</style>
